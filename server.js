09543065d084ca4c1bdfda4695bf20ffdd3a9b5d
Swapnil-2502 <hajareswapnil.2502@gmail.com>
2024-06-05 12:14:34 +0530

    Email func updated

--- server.js
+++ server.js
@@ -19,31 +19,38 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname)));
 
 app.post("/send_email", function(req,res){
-    const { fname, lname, email, cname, subject } = req.body;
+    try{
+        const { fname, lname, email, cname, subject } = req.body;
 
 
-    var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
-        }
-    })
-
-    const mailOptions = {
-        from: `${fname} ${lname} <${email}>`,
-        to: `${email}`, // Replace with the client's email
-        subject: 'New Contact Form Submission',
-        text: `First Name: ${fname}\nLast Name: ${lname}\nEmail: ${email}\nCountry: ${cname}\nSubject: ${subject}`
-      };
-
-    transporter.sendMail(mailOptions, function(error,info){
-        if(error){
-            console.log(error)
-        }else{
-            console.log("Email send: ", info.response)
-        }
-    })
+        var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS
+            }
+        })
+    
+        const mailOptions = {
+            from: `${fname} ${lname} <${email}>`,
+            to: `${email}`, // Replace with the client's email
+            subject: 'New Contact Form Submission',
+            text: `First Name: ${fname}\nLast Name: ${lname}\nEmail: ${email}\nCountry: ${cname}\nSubject: ${subject}`
+          };
+    
+        transporter.sendMail(mailOptions, function(error,info){
+            if(error){
+                console.log(error)
+            }else{
+                console.log("Email send: ", info.response)
+            }
+        })
+        res.json({ message: 'Email sent successfully' });
+    }catch(error){
+        console.error(error);
+        res.status(500).json({ message: 'Error while sending the email' });
+    }
+   
 })
 
 app.post("/send_email_wholesale", function(req,res){
@@ -68,10 +75,14 @@ app.post("/send_email_wholesale", function(req,res){
     transporter.sendMail(mailOptions, function(error,info){
         if(error){
             console.log(error)
+            res.status(500).json({message: 'Error while sending the email'});
         }else{
             console.log("Email send: ", info.response)
+            res.status(200).json({message: 'Email sent successfully'});
         }
     })
+
+    
 })
 
 server.listen(port, function(){
